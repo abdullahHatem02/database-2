08bf35b66e6c4a2d2eb6450f935589fbfb2b0c83
Abdullah Hatem <abdullahhatem87@yahoo.com>
2023-05-14 17:40:28 +0300

    Moved Helpers to BONUSHELPERS, all methods there are static, and should take the keyword this as a param to be able to call the create/select methods in DBapp

--- src/main/java/DBApp.java
+++ src/main/java/DBApp.java
@@ -797,130 +797,13 @@ public class DBApp {
 	    }
 	    
 	    switch(command){
-	    case "CREATE": if(tokens[1].equals("TABLE")) {createTableParser(tokens);}else{createIndexParser(tokens);};break;
-	    case "SELECT": selectParser(tokens);break;
-	    case "INSERT": insertParser(tokens);break;
+	    case "CREATE": if(tokens[1].equals("TABLE")) {BonusHelpers.createTableParser(tokens,this);}else{BonusHelpers.createIndexParser(tokens,this);};break;
+	    case "SELECT": BonusHelpers.selectParser(tokens,this);break;
+	    case "INSERT":  BonusHelpers.insertParser(tokens,this);break;
 	    }
 	     return null;
 	}
-	public void selectParser(String[] tokens) {
-		
-	}
-	public void insertParser(String[] tokens) {
-		String strTableName = tokens[2];
-		Hashtable<String,Object> htblColNameValue = new Hashtable<String,Object>();
-		Hashtable<String,String> dataTypes = new Hashtable<String,String>();
-		try {
-			dataTypes = StaticHelpers.getDataTypes(strTableName);
-		} catch (DBAppException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		String val = "";
-		boolean valuesCheck = false;
-		ArrayList<String> colNames = new ArrayList<String>();
-		ArrayList<String> values = new ArrayList<String>();
-		ArrayList<Object> valuesObj = new ArrayList<Object>();
-		for(int i = 3; i < tokens.length; i++) {
-			if(tokens[i].equals("VALUES")) {
-				valuesCheck = true;
-				continue;
-			}
-			if(valuesCheck) {
-				values.add(tokens[i]);
-				System.out.println(tokens[i] + " values");
-			}
-			else {
-				colNames.add(tokens[i]);
-				System.out.println(tokens[i] + " colNames");
-			}
-		
-		}
-		for(int j = 0; j < colNames.size(); j++) {
-			System.out.println(colNames.get(j) + "-----------");
-			val = dataTypes.get(colNames.get(j));
-			switch(val){
-			case "java.lang.Integer": valuesObj.add(Integer.parseInt(values.get(j)));continue;
-			case "java.lang.String": valuesObj.add(values.get(j));continue;	
-			case "java.lang.Double": valuesObj.add(Float.parseFloat(values.get(j)));continue;
-			case "java.lang.Date": SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-			try {
-				valuesObj.add(dateFormat.parse(values.get(j)));
-			} catch (ParseException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}continue;
-			}
-		}
-			for(int k = 0; k < colNames.size(); k++) {
-				
-				htblColNameValue.put(colNames.get(k), valuesObj.get(k));
-				System.out.println("colNames: "+ colNames.get(k) + " values:"+ valuesObj.get(k));
-			}
-			
-			System.out.println("Value of A: " + val);
-		
-		try {
-			insertIntoTable(strTableName, htblColNameValue);
-		} catch (DBAppException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-	       		
 	
-	
-	public void createTableParser(String[] tokens) throws DBAppException {
-		Hashtable <String,String>  htblColNameType = new Hashtable <String,String> ( );
-		Hashtable <String,String>  htblColNameMin = new Hashtable <String,String> ( );
-		Hashtable <String,String>  htblColNameMax = new Hashtable <String,String> ( );
-		
-		String colName;
-		String min = "";
-		String max = "";
-		String colType;
-		String tableName = tokens[2];
-		String clustering = "";
-		for(int i = 3 ; i < tokens.length;i++) {
-			if(tokens[i].equals("PRIMARY") && i+2 < tokens.length) {
-				clustering = tokens[i+2];
-				i+=3;
-			}
-			else {
-				colName = tokens[i];
-				colType = typeGetter(tokens[i+1]);
-				htblColNameType.put(colName, colType);
-				
-				if(i+5 < tokens.length) {
-				min = tokens[i+5];
-				System.out.println(min + " min");
-				}
-
-				if(i+9 < tokens.length) {
-				max = tokens[i+9];
-				System.out.println(max + " max");
-				}
-				
-				htblColNameMin.put(colName, min);
-				htblColNameMax.put(colName, max);
-				i = i+9;
-			}
-		}
-		createTable(tableName, clustering , htblColNameType, htblColNameMin, htblColNameMax);
-		
-	}
-	public String typeGetter(String type) {
-		//contains 3ashan lw fy bracket or comma or ay khara 
-		if(type.contains("VARCHAR"))
-			return "java.lang.String";
-		if(type.contains("INT"))
-			return "java.lang.Integer";
-		else 
-			return "java.lang.Double";	
-	}
-	public void createIndexParser(String[] tokens) {
-		
-	}
 
 	public static void main(String[] args) throws DBAppException, FileNotFoundException, IOException, ClassNotFoundException, ParseException {
 		DBApp x = new DBApp();
